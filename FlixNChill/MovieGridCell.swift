bc7f0d6ac75fe730529cf463579dcf2cf0c9f061
Eros Gonzalez <ErosGonzalez@eross-mbp.lan>
2019-02-07 00:20:06 -0800

    part 2

---
+++ FlixNChill/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixNChill
+//
+//  Created by Eros Gonzalez on 2/6/19.
+//  Copyright © 2019 Eros Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
